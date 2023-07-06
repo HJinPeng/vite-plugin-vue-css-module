2d7821de967e6799214f39ea64cc0abb3276ca3e
jinpengh <742463015@qq.com>
2023-07-06 14:36:18 +0800

    style: 删除console

--- src/index.ts
+++ src/index.ts
@@ -21,11 +21,9 @@ export default function vueCssModule(userOptions: Partial<PluginOptions> = {}): 
     name: 'vite-plugin-vue-css-module',
     enforce: 'pre',
     async transform(code: string, id: string) {
-      // console.log('code', code);
       if (id.endsWith('.vue')) {
         // style标签上有module，表示开启了css-module
         if (/<style[\s\S]* module[\s\S]*>[\s\S]*<\/style>/.test(code)) {
-          console.log('!!')
           const templateAst = getTemplateAst(code)
           if (!templateAst || templateAst.children?.length === 0) return
           const s = new MagicString(code)
