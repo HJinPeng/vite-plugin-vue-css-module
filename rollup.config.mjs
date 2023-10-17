c47561e9830bee3ab2e8cfc70d78e630055223b2
jinpengh <742463015@qq.com>
2023-10-17 23:34:29 +0800

    fix: 修复index.d.ts声明文件问题,依赖位置调整

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,43 +1,36 @@
 import typescript from '@rollup/plugin-typescript'
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.cjs',
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: 'dist/index.mjs',
-      format: 'es',
-      sourcemap: true
-    },
-    {
-      file: 'dist/index.umd.js',
-      format: 'umd',
-      name: 'vite-plugin-vue-css-module',
-      globals: {
-        'magic-string': 'MagicString',
-        '@vue/compiler-sfc': 'CompilerSfc',
-        '@babel/parser': 'BabelParser',
-        '@babel/generator': 'BabelGenerator',
-        '@babel/traverse': 'BabelTraverse',
-        '@babel/types': 'BabelTypes'
+import { dts } from 'rollup-plugin-dts'
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        sourcemap: true
       },
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    typescript()
-  ],
-  external: [
-    'magic-string', 
-    '@vue/compiler-sfc', 
-    '@vue/compiler-core',
-    'pug-parser',
-    'pug-lexer',
-    'pug-walk',
-    'pug-runtime/wrap',
-    'pug-code-gen'
-  ]
-}
+      {
+        file: 'dist/index.mjs',
+        format: 'es',
+        sourcemap: true
+      }
+    ],
+    plugins: [typescript()],
+    external: [
+      'magic-string',
+      '@vue/compiler-sfc',
+      '@vue/compiler-core',
+      'pug-parser',
+      'pug-lexer',
+      'pug-walk',
+      'pug-runtime/wrap',
+      'pug-code-gen'
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()]
+  }
+]
