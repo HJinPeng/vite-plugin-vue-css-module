264d4664b9cec41a90901cd66bc4886175a3cdfa
Ilya Semenov <ilya@semenov.co>
2024-03-28 15:28:47 +0700

    Support pug HMR in Vite 5 (fix #9)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import type { Plugin } from 'vite'
 import type { PluginOptions } from './utils/types'
-import { MagicString } from 'vue/compiler-sfc'
+import { MagicString, type SFCTemplateBlock } from 'vue/compiler-sfc'
 import { parseVue } from './utils/parseVue'
 import { parseHtml } from './utils/parseHtml'
 import { parsePug } from './utils/parsePug'
-import { isLegalVariate } from './utils/tool'
 
 export default function vueCssModule(userOptions: Partial<PluginOptions> = {}): Plugin {
   const options = {
@@ -12,41 +11,89 @@ export default function vueCssModule(userOptions: Partial<PluginOptions> = {}): 
     pugClassLiterals: false,
     ...userOptions
   }
+
+  const cache = new Map<
+    string,
+    {
+      code: string
+      template: SFCTemplateBlock | null
+      cssModule?: string | boolean
+      mustTransform?: boolean
+    }
+  >()
+
+  async function transformTemplate({
+    code,
+    template,
+    cssModule
+  }: {
+    code: string
+    template: SFCTemplateBlock
+    cssModule: string | true
+  }) {
+    const cssModuleName = cssModule === true ? '$style' : cssModule
+    const s = new MagicString(code)
+
+    if (template.lang === 'pug') {
+      const result = await parsePug(template.content, options, cssModuleName)
+      s.update(template.loc.start.offset, template.loc.end.offset, result)
+      return s
+    } else if (template.ast?.children.length) {
+      parseHtml(template.ast.children, s, options.attrName, cssModuleName)
+      return s
+    }
+  }
+
   return {
     name: 'vite-plugin-vue-css-module',
     enforce: 'pre',
     async transform(code: string, id: string) {
-      if (id.endsWith('.vue')) {
-        // style标签上有module，表示开启了css-module
-        const { cssModule, template } = parseVue(code)
-        if (cssModule && template && template.content) {
-          const { ast: templateAst, content: templateContent, lang } = template
-          const cssModuleName = cssModule === true ? '$style' : cssModule
-          // cssModuleName 需要是合法变量名
-          if (!isLegalVariate(cssModuleName)) {
-            throw new Error(`module的值不能是${cssModuleName}，它必须是一个合法变量名`)
-          }
-          const s = new MagicString(code)
-
-          if (!templateAst || templateAst.children.length === 0) {
-            return
-          }
-
-          if (lang === 'pug') {
-            const templateOffset = [template.loc.start.offset, template.loc.end.offset]
-            const result = await parsePug(templateContent, options, cssModuleName)
-            // 将pug模板中的属性为 attrName 的值转为 cssModule写法，转为html模板
-            s.update(templateOffset[0], templateOffset[1], result)
-            return {
-              code: s.toString(),
-              map: s.generateMap()
+      const [file, queryString] = id.split('?')
+      if (file.endsWith('.vue')) {
+        const params = new URLSearchParams(queryString)
+        const type = params.get('type')
+        if (id === file) {
+          // Full SFC transform
+          const cached = cache.get(file)
+          const { template, cssModule } = cached?.code === code ? cached : parseVue(code)
+          cache.set(file, { code, template, cssModule })
+          if (template && cssModule) {
+            const s = await transformTemplate({ code, template, cssModule })
+            if (s) {
+              return { code: s.toString(), map: s.generateMap() }
             }
           }
-
-          parseHtml(templateAst.children, s, options.attrName, cssModuleName)
-          return {
-            code: s.toString(),
-            map: s.generateMap()
+        } else if (type === 'template') {
+          // Template transform (for HMR)
+          const cached = cache.get(file)
+          if (cached?.mustTransform) {
+            cached.mustTransform = false
+            const { code, template, cssModule } = cached
+            if (template && cssModule) {
+              const s0 = await transformTemplate({ code, template, cssModule })
+              if (s0) {
+                const s = s0.snip(template.loc.start.offset, template.loc.end.offset)
+                return { code: s.toString(), map: s.generateMap() }
+              }
+            }
+          }
+        }
+      }
+    },
+    async handleHotUpdate({ file, read }) {
+      if (file.endsWith('.vue')) {
+        const cached = cache.get(file)
+        if (cached) {
+          const code = await read()
+          const { template, cssModule } = parseVue(code)
+          if (template) {
+            const mustTransform = !(
+              cached.template &&
+              cached.template.content === template.content &&
+              cached.template.lang === template.lang &&
+              cached.cssModule === cssModule
+            )
+            cache.set(file, { code, template, cssModule, mustTransform })
           }
         }
       }
